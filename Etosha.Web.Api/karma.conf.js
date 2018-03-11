408b2f8303e47f99b94a594ee79d54e1a574e6ac
Dominic Sachs <d.sachs@gmx.de>
2018-03-11 12:16:48 +0100

    Fix test uploads

--- Etosha.Web.Api/karma.conf.js
+++ Etosha.Web.Api/karma.conf.js
@@ -27,10 +27,9 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
+    autoWatch: true,
     browsers: ['ChromeHeadless'],
-    singleRun: false,
-    concurrency: Infinity,
+    singleRun: true,
     junitReporter: {
       outputDir: 'ng-tests',
       outputFile: 'ng-test-results.xml',
