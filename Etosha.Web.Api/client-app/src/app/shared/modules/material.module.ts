dc86e089fcde22c0197536dd54fee636bde1bbb7
Dominic <d.sachs@gmx.de>
2020-04-15 22:02:12 +0200

    updates

--- Etosha.Web.Api/client-app/src/app/shared/modules/material.module.ts
+++ Etosha.Web.Api/client-app/src/app/shared/modules/material.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
-import {
-    MatButtonModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatSortModule,
-    MatTableModule,
-    MatToolbarModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
     imports: [
--- Etosha.Web.Api/client-app/src/app/stocks/stocks-board.component.ts
+++ Etosha.Web.Api/client-app/src/app/stocks/stocks-board.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
+import { MatTableDataSource } from '@angular/material/table';
 import { HubConnection, HubConnectionBuilder, LogLevel } from '@aspnet/signalr';
 import { environment } from '../../environments/environment';
 import { Stock } from './stock.model';
